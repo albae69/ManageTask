9296405785f5d2cef28a7e8ec272444aa042e2d7
albae69 <ahmadalbaihaqi69@gmail.com>
2020-11-12 22:35:07 +0700

    initial commit

---
+++ src/store/Slice.js
@@ -0,0 +1,25 @@
+  
+import {createSlice} from '@reduxjs/toolkit';
+
+const Auth = {
+isLogin:false,
+  user: null,
+  token: null,
+};
+
+const auth = createSlice({
+  name: 'auth',
+  initialState: Auth,
+  reducers: {
+    login: (state) => {
+      state.isLogin= true;
+    },
+    logout: (state) => {
+      state.isLogin = false;
+    },
+  },
+});
+
+export const {login, logout} = auth.actions;
+
+export default auth.reducer;
